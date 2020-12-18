f45fd710877a7768099005161e969652e67734bc
Lisa Poncet <poncet.lisa@gmail.com>
2020-12-18 16:21:44 +0100

    use hooks instead of classes on app.js

--- src/App.js
+++ src/App.js
@@ -1,71 +1,63 @@
 import "./stylesheets/App.css";
 import RandomQuote from "./component/RandomQuote";
 import Navbar from "./component/Navbar";
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
 import axios from "axios";
 import AuthorQuotes from "./component/AuthorQuotes";
 
-class App extends Component {
-  state = {
-    randomQuote: "",
-    authorQuotes: [],
-  };
+const App = () => {
+  const [randomQuote, setRandomQuote] = useState("");
+  const [authorQuotes, setAuthorQuotes] = useState([]);
 
-  componentDidMount() {
-    this.getRandomQuote();
-  }
+  useEffect(() => {
+    getRandomQuote();
+  }, []);
 
-  getRandomQuote = () => {
+  const getRandomQuote = () => {
     axios
       .get("https://quote-garden.herokuapp.com/api/v3/quotes/random")
       .then((apiRes) => {
-        this.setState({
-          randomQuote: apiRes.data.data[0],
-        });
+        setRandomQuote(apiRes.data.data[0]);
       })
       .catch((err) => console.log(err));
   };
 
-  getAuthorQuotes = (name) => {
+  const getAuthorQuotes = (name) => {
     axios
       .get(`https://quote-garden.herokuapp.com/api/v3/quotes?author=${name}`)
       .then((apiRes) => {
-        this.setState({
-          authorQuotes: apiRes.data.data,
-        });
+        setAuthorQuotes(apiRes.data.data);
       })
       .catch((err) => console.log(err));
   };
 
-  render() {
-    const random = this.state.randomQuote;
-    return (
-      <>
-        <Navbar randomize={this.getRandomQuote} />
-        <Switch>
-          <Route exact path="/">
-            <div className="App">
-              <RandomQuote
-                quote={random.quoteText}
-                author={random.quoteAuthor}
-                genre={random.quoteGenre}
-                authorQuotes={this.getAuthorQuotes}
-              />
-            </div>
-          </Route>
-          <Route path="/author/:name">
-            <AuthorQuotes
-              getQuotes={this.getAuthorQuotes}
+  const random = randomQuote;
+  return (
+    <>
+      <Navbar randomize={getRandomQuote} />
+      <Switch>
+        <Route exact path="/">
+          <div className="App">
+            <RandomQuote
+              quote={random.quoteText}
               author={random.quoteAuthor}
-              quotes={this.state.authorQuotes}
+              genre={random.quoteGenre}
+              authorQuotes={getAuthorQuotes}
             />
-          </Route>
-        </Switch>
-        <footer>Lisa Poncet @ Devchallenges.io</footer>
-      </>
-    );
-  }
-}
+          </div>
+        </Route>
+        <Route path="/author/:name">
+          <AuthorQuotes
+            getQuotes={getAuthorQuotes}
+            author={random.quoteAuthor}
+            quotes={authorQuotes}
+          />
+        </Route>
+      </Switch>
+      <footer>Lisa Poncet @ Devchallenges.io</footer>
+    </>
+  );
+};
 
 export default App;
